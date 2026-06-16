f3a637ec60ae9785066733be387ed51a6945066a
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-16 23:09:20 +0300

    [4]: add new roles to db_init.sql

--- db_init.sql
+++ db_init.sql
@@ -1,4 +1,7 @@
 CREATE DATABASE inventorydb;
 CREATE ROLE app_user WITH LOGIN PASSWORD 'gfhjkm';
+CREATE ROLE catalog_manager WITH LOGIN PASSWORD 'catalog';
+CREATE ROLE sales_manager WITH LOGIN PASSWORD 'sales';
+CREATE ROLE supervisor WITH LOGIN PASSWORD 'password';
 GRANT CREATE ON DATABASE inventorydb TO app_user;
 
