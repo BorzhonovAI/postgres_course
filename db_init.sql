e56a821f11f80a3554fcd0b6c54780d4464cc1fb
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-17 22:58:29 +0300

    [4]: update init sql scripts

--- db_init.sql
+++ db_init.sql
@@ -1,7 +1,4 @@
 CREATE DATABASE inventorydb;
-CREATE ROLE app_user WITH LOGIN PASSWORD 'gfhjkm';
-CREATE ROLE catalog_manager WITH LOGIN PASSWORD 'catalog';
-CREATE ROLE sales_manager WITH LOGIN PASSWORD 'sales';
-CREATE ROLE supervisor WITH LOGIN PASSWORD 'password';
+CREATE ROLE app_user WITH CREATEROLE LOGIN PASSWORD 'gfhjkm';
 GRANT CREATE ON DATABASE inventorydb TO app_user;
 
--- wsl_init.sh
+++ wsl_init.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-cd /home/anatoly/work/projects/postgres_course
+cd /home/anatoly/work/postgres_db
 docker compose up -d
 docker cp /home/anatoly/work/projects/postgres_course/db_init.sql db:/tmp/
+docker cp /home/anatoly/work/projects/postgres_course/roles_init.sql db:/tmp/
 docker cp /home/anatoly/work/projects/postgres_course/alembic_init.sql db:/tmp/
 docker exec -it db psql -U postgres -f /tmp/db_init.sql
 docker exec -it db psql -U postgres -d inventorydb -f /tmp/alembic_init.sql
-cd /home/anatoly/work/projects/postgres_course/pg-course-template &&
-python3 -m venv .venv &&
-source .venv/bin/activate &&
-pip install -r requirements.txt &&
-alembic downgrade base &&
-alembic upgrade head
+docker exec -it db psql -U app_user -d inventorydb -f /tmp/roles_init.sql
