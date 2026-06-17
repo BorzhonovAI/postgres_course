bc9147f2dd9d12b735ae78e0f651160703b6ac83
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-17 22:57:49 +0300

    [4]: add migration for roles permissions

---
+++ pg-course-template/alembic/sql/3afa6f8ee004/up.sql
@@ -0,0 +1,17 @@
+ALTER DEFAULT PRIVILEGES IN SCHEMA "catalog" GRANT SELECT ON TABLES TO PUBLIC;
+
+GRANT ALL ON SCHEMA "catalog" TO catalog_manager;
+GRANT ALL ON ALL TABLES IN SCHEMA "catalog" TO catalog_manager;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA "catalog" TO catalog_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA "catalog" GRANT ALL ON TABLES TO catalog_manager;
+
+GRANT ALL ON SCHEMA sales TO sales_manager;
+GRANT ALL ON ALL TABLES IN SCHEMA sales TO sales_manager;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA sales TO sales_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA sales GRANT ALL ON TABLES TO sales_manager;
+GRANT USAGE ON SCHEMA "catalog" TO sales_manager;
+GRANT SELECT ON ALL TABLES in schema "catalog" TO sales_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA "catalog" GRANT SELECT ON TABLES TO sales_manager;
+
+GRANT sales_manager TO supervisor;
+GRANT catalog_manager TO supervisor;
