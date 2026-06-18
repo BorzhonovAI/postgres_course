bf69ac60bfa61a8551fdd65bd5c1130185750ef2
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-18 23:39:01 +0300

    [4]: add col id for auth.users

--- auth_init.sql
+++ auth_init.sql
@@ -1,16 +1,16 @@
 CREATE SCHEMA auth AUTHORIZATION app_user;
 CREATE TABLE auth.users (
+    id SERIAL PRIMARY KEY,
 	username text NOT NULL,
 	"password" text NOT NULL,
 	"role" text NOT NULL,
-	CONSTRAINT users_pk PRIMARY KEY (username),
 	CONSTRAINT role_check CHECK (role in ('catalog_manager', 'sales_manager'))
 );
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 --для простоты пароли пользователей будут совпадать с паролями ролей
-insert into auth.users (username, password, role) VALUES
+INSERT INTO auth.users (username, password, role) VALUES
 ('sal', crypt('sales', gen_salt('bf')), 'sales_manager');
-insert into auth.users (username, password, role) VALUES
+INSERT INTO auth.users (username, password, role) VALUES
 ('cat', crypt('catalog', gen_salt('bf')), 'catalog_manager');
 
