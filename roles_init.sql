268f23d88495a62d14ab728cefa32bfac55b4787
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-23 22:25:52 +0300

    [5]: add roles inventory_manager, worker

--- roles_init.sql
+++ roles_init.sql
@@ -1,4 +1,6 @@
 CREATE ROLE catalog_manager WITH LOGIN PASSWORD 'catalog';
 CREATE ROLE sales_manager WITH LOGIN PASSWORD 'sales';
 CREATE ROLE supervisor WITH LOGIN PASSWORD 'password';
+CREATE ROLE inventory_manager WITH LOGIN PASSWORD 'inventory';
+CREATE ROLE worker WITH LOGIN PASSWORD 'worker';
 
