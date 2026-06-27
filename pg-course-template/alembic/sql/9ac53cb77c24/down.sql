07da3662ffbcf7c59281e1fbb9f6a11e6d303c43
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-27 19:22:24 +0300

    [5]: add missing inventory tables migration

---
+++ pg-course-template/alembic/sql/9ac53cb77c24/down.sql
@@ -0,0 +1,4 @@
+DROP TABLE inventory.transfer_items;
+DROP TABLE inventory.transfers;
+DROP TABLE inventory.delivery_items;
+DROP TABLE inventory.reserves;
---
+++ pg-course-template/alembic/versions/9ac53cb77c24_5_add_missing_inventory_tables.py
@@ -0,0 +1,28 @@
+"""5_add_missing_inventory_tables
+
+Revision ID: 9ac53cb77c24
+Revises: e34dcb9ce90d
+Create Date: 2026-06-27 18:05:49.984839
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9ac53cb77c24'
+down_revision: Union[str, None] = 'e34dcb9ce90d'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with open(f"alembic/sql/{revision}/up.sql") as file:
+        op.execute(file.read())
+
+
+def downgrade() -> None:
+    with open(f"alembic/sql/{revision}/down.sql") as file:
+        op.execute(file.read())
