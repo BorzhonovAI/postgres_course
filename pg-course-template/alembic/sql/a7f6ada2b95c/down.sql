f05abf47b8a5628d5dcbf0b8869dbc6b3c0d7943
BorzhonovAI <tolyaborzhonov@gmail.com>
2026-06-27 19:22:31 +0300

    [5]: add processing_by_id column to orders

---
+++ pg-course-template/alembic/sql/a7f6ada2b95c/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE sales.orders DROP CONSTRAINT orders_processing_by_fk;
+ALTER TABLE sales.orders DROP COLUMN processing_by_id;
---
+++ pg-course-template/alembic/versions/a7f6ada2b95c_5_add_processing_by_id_to_orders.py
@@ -0,0 +1,28 @@
+"""5_add_processing_by_id_to_orders
+
+Revision ID: a7f6ada2b95c
+Revises: 9ac53cb77c24
+Create Date: 2026-06-27 18:07:25.887138
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'a7f6ada2b95c'
+down_revision: Union[str, None] = '9ac53cb77c24'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    with open(f"alembic/sql/{revision}/up.sql") as file:
+        op.execute(file.read())
+
+
+def downgrade() -> None:
+    with open(f"alembic/sql/{revision}/down.sql") as file:
+        op.execute(file.read())
